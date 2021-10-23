f1da4ba60a096550c5bbbe1538c55ccbaafa9fe1
jonko0493 <jonko0493@protonmail.com>
2021-10-22 21:45:24 -0700

    Initial commit

---
+++ HaruhiChokuretsuEditor/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HaruhiChokuretsuEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private EventFile _event;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenEventsFileButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveEventsFileButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
