64c733106839f68b9cf6829bb55b0f813963ac40
jonko0493 <jonko0493@protonmail.com>
2021-10-24 17:48:22 -0700

    Add tests, initial ASM implementation of decompression routine

---
+++ HaruhiChokuretsuTests/CompressionTests.cs
@@ -0,0 +1,32 @@
+using HaruhiChokuretsuEditor;
+using NUnit.Framework;
+using System.IO;
+
+namespace HaruhiChokuretsuTests
+{
+    public class CompressionTests
+    {
+        [Test]
+        [TestCase(TestVariables.EVT_000_COMPRESSED, TestVariables.EVT_000_DECOMPRESSED)]
+        public void AsmSimulatorTest(string compressedFile, string decompressedFile)
+        {
+            byte[] compressedData = File.ReadAllBytes(compressedFile);
+            AsmDecompressionSimulator asm = new(compressedData);
+
+            byte[] decompressedDataOnDisk = File.ReadAllBytes(decompressedFile);
+            File.WriteAllBytes(".\\inputs\\ev_000_asm_decomp.bin", asm.Output);
+            Assert.AreEqual(decompressedDataOnDisk, asm.Output);
+        }
+
+        [Test]
+        [TestCase(TestVariables.EVT_000_COMPRESSED, TestVariables.EVT_000_DECOMPRESSED)]
+        public void DecompressionMethodTest(string compressedFile, string decompressedFile)
+        {
+            byte[] decompressedDataInMemory = Helpers.DecompressData(File.ReadAllBytes(compressedFile));
+            File.WriteAllBytes(".\\inputs\\ev_000_prog_decomp.bin", decompressedDataInMemory);
+
+            byte[] decompressedDataOnDisk = File.ReadAllBytes(decompressedFile);
+            Assert.AreEqual(decompressedDataOnDisk, decompressedDataInMemory);
+        }
+    }
+}
---
+++ HaruhiChokuretsuTests/TestVariables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaruhiChokuretsuTests
+{
+    public static class TestVariables
+    {
+        public const string EVT_000_COMPRESSED = ".\\inputs\\ev_000_compressed.bin";
+        public const string EVT_000_DECOMPRESSED = ".\\inputs\\ev_000_decompressed.bin";
+    }
+}
