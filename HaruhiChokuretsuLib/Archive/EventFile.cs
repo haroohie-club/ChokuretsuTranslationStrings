bb987308c69fb54ab2fba68bf64356ec4b6265ca
jonko0493 <jonko0493@protonmail.com>
2021-11-16 15:40:59 -0800

    5 char -> 15 char

--- HaruhiChokuretsuLib/Archive/EventFile.cs
+++ HaruhiChokuretsuLib/Archive/EventFile.cs
@@ -323,7 +323,7 @@ namespace HaruhiChokuretsuLib.Archive
 
                 if (dialogueText.Count(c => c == '\n') > 1)
                 {
-                    Console.WriteLine($"File {Index} has dialogue line too long ({dialogueIndex}) (starting with: {dialogueText[0..Math.Min(5, dialogueText.Length - 1)]})");
+                    Console.WriteLine($"File {Index} has dialogue line too long ({dialogueIndex}) (starting with: {dialogueText[0..Math.Min(15, dialogueText.Length - 1)]})");
                 }
 
                 EditDialogueLine(dialogueIndex, dialogueText);
