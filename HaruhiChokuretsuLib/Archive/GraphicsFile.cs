bc571c68de67bed624da161e94af34dca97a5fae
jonko0493 <jonko0493@protonmail.com>
2022-02-15 23:05:11 -0800

    More graphics adjustments

--- HaruhiChokuretsuLib/Archive/GraphicsFile.cs
+++ HaruhiChokuretsuLib/Archive/GraphicsFile.cs
@@ -96,7 +96,7 @@ namespace HaruhiChokuretsuLib.Archive
                 && Index != 0x3AF && Index != 0x3FE
                 && (Index < 0x41B || Index > 0x42C)
                 && (Index < 0x8B4 || Index > 0x8B7)
-                && (Index < 0xB61 || Index > 0xB7C)
+                && (Index < 0xB61 || Index > 0xB6E)
                 && (Index < 0xBC9 || Index > 0xC1B)
                 && (Index < 0xC70 || Index > 0xC78)
                 && (Index < 0xCA3 || Index > 0xCA8)
